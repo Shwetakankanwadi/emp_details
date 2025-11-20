f2b2ab5c800a448f9f1075e49e4c5b705b630271
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-11-20 15:50:28 +0530

    Update emp.py

--- emp.py
+++ emp.py
@@ -1,11 +1,11 @@
 import sys
 script_name=sys.argv[0]
-EMP_name=sys.argv[1]
+emp_name=sys.argv[1]
 salary= sys.argv[2]
 id= sys.argv[3]
 exp=sys.argv[4]
 print("script name:",script_name)
-print("Employee name:",EMP_name)
+print("Employee name:",emp_name)
 print("Salary:",salary)
 print("ID:",id)
 print("expiriance:",exp)
